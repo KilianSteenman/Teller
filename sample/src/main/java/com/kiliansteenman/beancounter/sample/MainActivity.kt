cb5e1e2e489e91ff46272a9367bb558f20c23ef5
Kilian Steenman <37879201+KilianSteenman@users.noreply.github.com>
2019-08-24 10:29:53 +0200

    Initial project setup

---
+++ sample/src/main/java/com/kiliansteenman/beancounter/sample/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.kiliansteenman.beancounter.sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kiliansteenman.beancounter.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
