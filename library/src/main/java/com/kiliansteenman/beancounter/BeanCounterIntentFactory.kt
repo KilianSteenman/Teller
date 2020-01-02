72187d7c62cabb7fb7543a9d858edf0326f43406
Kilian Steenman <37879201+KilianSteenman@users.noreply.github.com>
2020-01-02 19:15:11 +0100

    Change BeanCounter launch mode

--- library/src/main/java/com/kiliansteenman/beancounter/BeanCounterIntentFactory.kt
+++ library/src/main/java/com/kiliansteenman/beancounter/BeanCounterIntentFactory.kt
@@ -7,6 +7,8 @@ import com.kiliansteenman.beancounter.internal.ui.BeanCounterActivity
 object BeanCounterIntentFactory {
 
     fun createIntent(context: Context): Intent? {
-        return Intent(context, BeanCounterActivity::class.java)
+        return Intent(context, BeanCounterActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        }
     }
 }
