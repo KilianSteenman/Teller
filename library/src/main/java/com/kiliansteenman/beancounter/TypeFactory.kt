048215dadf6e8d9aad4a0da621a767e017cd33e7
Kilian Steenman <37879201+KilianSteenman@users.noreply.github.com>
2020-01-04 18:42:21 +0100

    Make TypeFactory internal

--- library/src/main/java/com/kiliansteenman/beancounter/TypeFactory.kt
+++ library/src/main/java/com/kiliansteenman/beancounter/TypeFactory.kt
@@ -1,17 +1,11 @@
 package com.kiliansteenman.beancounter
 
-import kotlin.reflect.KClass
-
-class TypeFactory {
+internal class TypeFactory {
 
     private val mapping: MutableMap<String, AnalyticsAdapter<*>> = mutableMapOf()
 
     fun addMapping(name: String, adapter: AnalyticsAdapter<*>) {
         mapping[name] = adapter
-    }
-
-    fun <T : Any> getAdapterForType(bean: KClass<T>): AnalyticsAdapter<T> {
-        return mapping[bean.toString()] as AnalyticsAdapter<T>
     }
 
     fun getAdapterForType(name: String?): AnalyticsAdapter<*>? {
