cc4f25d67a818a1a8731676372f8874cd18cd158
Kilian Steenman <37879201+KilianSteenman@users.noreply.github.com>
2022-07-17 20:38:31 +0200

    Add Adobe Framework

---
+++ frameworks/adobe/src/main/java/com/kiliansteenman/teller/framework/AdobeFramework.kt
@@ -0,0 +1,19 @@
+package com.kiliansteenman.teller.framework
+
+import android.util.Log
+import com.adobe.marketing.mobile.MobileCore
+import com.kiliansteenman.teller.Framework
+import com.kiliansteenman.teller.Measurement
+
+class AdobeFramework : Framework {
+
+    override val name: String = NAME
+
+    override fun count(measurement: Measurement) {
+        MobileCore.trackAction(measurement.name, measurement.params.mapValues { it.toString() })
+    }
+
+    companion object {
+        const val NAME = "Adobe"
+    }
+}
