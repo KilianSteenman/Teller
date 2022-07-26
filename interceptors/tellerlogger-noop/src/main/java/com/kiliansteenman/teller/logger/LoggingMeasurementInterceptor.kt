0df38660241f004135d6bb68730eacdff9f6db94
Kilian Steenman <37879201+KilianSteenman@users.noreply.github.com>
2022-07-26 20:10:53 +0200

    Make sure lib builds when using Flipper

--- interceptors/tellerlogger-noop/src/main/java/com/kiliansteenman/teller/logger/LoggingMeasurementInterceptor.kt
+++ interceptors/tellerlogger-noop/src/main/java/com/kiliansteenman/teller/logger/LoggingMeasurementInterceptor.kt
@@ -1,8 +1,10 @@
 package com.kiliansteenman.teller.logger
 
+import android.content.Context
 import com.kiliansteenman.teller.Measurement
 import com.kiliansteenman.teller.MeasurementInterceptor
 
-class LoggingMeasurementInterceptor : MeasurementInterceptor {
+@Suppress("UnusedParameter")
+class LoggingMeasurementInterceptor(context: Context) : MeasurementInterceptor {
     override fun process(measurement: Measurement): Measurement = measurement
 }
